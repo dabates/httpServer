e9fab2f39facdef2c729c6e7a6f7439ad3a03edb
David Bates <me@dave-bates.net>
2025-04-04 06:44:58 -0500

    New app - Adds the new module and main app - Adds ability to serve files from the disk

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dabates/httpServer
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	httpServer.ListenAndServe()
+}
